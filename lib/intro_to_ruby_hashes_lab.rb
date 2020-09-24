d6d481f5ac7cf3412c89765a4e692e4e6c4f2eee
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-24 04:01:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,5 @@
 def my_hash_creator(key, value)
   {key => value}
-  
 end
 
 def read_from_hash(hash, key)
@@ -8,5 +7,5 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key]? 
+  hash[key]? hash[key] += 1
 end
